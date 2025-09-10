b55be277d4fe1493430f06d49f1891887eeb7da3
viniciusmeireles <viniciusmeireles@gmail.com>
2025-09-10 17:43:58 +0000

    Semana 5

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'firebase_options.dart';
 import 'login.dart';
 import 'welcome.dart';
+import 'notes.dart';
 
 Future<void> main() async {
  WidgetsFlutterBinding.ensureInitialized();
@@ -48,12 +49,30 @@ class HomePage extends StatelessWidget {
 
  @override
  Widget build(BuildContext context) {
+   final user = FirebaseAuth.instance.currentUser;
    return Scaffold(
      appBar: AppBar(title: const Text("Home")),
      drawer: Drawer(
        child: ListView(
          padding: EdgeInsets.zero,
          children: [
+           UserAccountsDrawerHeader(
+             accountName: Text(user?.displayName ?? "Usuário"),
+             accountEmail: Text(user?.email ?? ""),
+             currentAccountPicture: const CircleAvatar(
+               child: Icon(Icons.person),
+             ),
+           ),
+           ListTile(
+             leading: const Icon(Icons.note),
+             title: const Text('Anotações'),
+             onTap: () async {
+               Navigator.push(
+                 context,
+                 MaterialPageRoute(builder: (_) => const NotesPage()),
+               );
+             },
+           ),
            ListTile(
              leading: const Icon(Icons.logout),
              title: const Text('Sair'),
